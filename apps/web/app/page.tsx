a2764514c1ce035cadd8d794bd0307835586f482
leehyungwon <hueleev@dkitec.com>
2025-05-12 18:20:30 +0900

    button update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const handleClick = () => {
+    window.location.href = "/storybook";
+  };
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Button size="sm" onClick={handleClick}>
+          Storybook
+        </Button>
       </div>
     </div>
-  )
+  );
 }
