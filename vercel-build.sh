c434eae1282c6d9cd5c05690c147a0aff12fc263
leehyungwon <hueleev@dkitec.com>
2025-07-18 16:35:30 +0900

    sh 수정

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "DATABASE_URL=$DATABASE_URL" # 디버깅용 출력
 pnpm --filter ui build-storybook
 if [ -d packages/ui/storybook-static ]; then
   cp -r packages/ui/storybook-static apps/web/public/storybook
